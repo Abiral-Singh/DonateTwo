262bcf01c964636ac1ec275735f5332e1f9c5563
Abiral Singh <abiral-singh@users.noreply.github.com>
2019-04-25 22:19:48 +0530

    Post page Ui updated.
    Signed-off-by: Abiral Singh <Abiral-Singh@users.noreply.github.com>

--- app/src/main/java/com/abiralsingh/donatetwo/MyAdapter.java
+++ app/src/main/java/com/abiralsingh/donatetwo/MyAdapter.java
@@ -1,11 +1,15 @@
 package com.abiralsingh.donatetwo;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -15,6 +19,8 @@ import com.google.firebase.database.DataSnapshot;
 public class MyAdapter extends FirebaseRecyclerAdapter<Donate_Item, MyAdapter.MyViewHolder> {
 
     private OnItemClickListener listener;
+    Context applicatioContext;
+    int delay=100;
 
     /**
      * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
@@ -26,12 +32,21 @@ public class MyAdapter extends FirebaseRecyclerAdapter<Donate_Item, MyAdapter.My
 
         super(options);
     }
+    public MyAdapter(@NonNull FirebaseRecyclerOptions<Donate_Item> options, Context context) {
+        super(options);
+        applicatioContext =context;
+    }
 
     @Override
     protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Donate_Item model) {
         holder.textViewPost_title.setText(model.getPostTitle());
         holder.textViewPost_tag.setText(model.getTag());
         holder.textViewPost_desc.setText(model.getPostDesc()) ;
+
+        Animation anim = AnimationUtils.loadAnimation(applicatioContext,R.anim.text_box_entry);
+        anim.setStartOffset(delay);
+        holder.relativeLayout.startAnimation(anim);
+        delay=delay+200;
     }
 
     @NonNull
@@ -43,7 +58,7 @@ public class MyAdapter extends FirebaseRecyclerAdapter<Donate_Item, MyAdapter.My
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout relativeLayout;
         TextView textViewPost_title;
         TextView textViewPost_tag;
         TextView textViewPost_desc;
@@ -51,6 +66,7 @@ public class MyAdapter extends FirebaseRecyclerAdapter<Donate_Item, MyAdapter.My
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            relativeLayout = itemView.findViewById(R.id.custom_item_relative_layout);
             textViewPost_title = itemView.findViewById(R.id.text_post_title);
             textViewPost_tag = itemView.findViewById(R.id.text_post_tag);
             textViewPost_desc = itemView.findViewById(R.id.text_post_desc);
